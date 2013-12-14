68d8a0163b7bbcdc10b4bace73b766d3cd47020f
Shannon <shannon.r.luke@gmail.com>
2013-12-14 13:51:39 +1000

    Updated -sh

--- string_reverse-Sh.py
+++ string_reverse-Sh.py
@@ -4,3 +4,7 @@ Reversing a string
 
 text = raw_input("Please enter a string: ")
 print "Your entered string is: ", text
+for i in text:
+	text_length = 0
+	text_length = text_length + 1
+	print text_length
