2fccb48e03ff8ffa0bcf05b4de2004b28e5aa723
Shannon <shannon.r.luke@gmail.com>
2013-12-14 14:33:48 +1000

    Fixed it all

--- string_reverse-Sh.py
+++ string_reverse-Sh.py
@@ -4,8 +4,7 @@ Reversing a string
 
 text = raw_input("Please enter a string: ")
 characters = [i for i in text]
-reversed_characters=[]
-for i in range(len(text)-1):
-		reversed_characters.insert(0, characters[i])
-
-print "Your reversed string is :", characters
+reversed_characters=""
+for i in range(len(text) - 1, -1, -1):
+		reversed_characters += characters[i]
+print "Your reversed string is :", reversed_characters
