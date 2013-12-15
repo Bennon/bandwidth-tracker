a7fced73ae506628312054043f34db8d6f031bea
Shannon <shannon.r.luke@gmail.com>
2013-12-15 10:58:59 +1000

    pig latin mult updated

--- pig_latin_multiple.py
+++ pig_latin_multiple.py
@@ -3,3 +3,30 @@ Pig latin generator (multiple words)
 Probably need to store each word in an array and modify
 in a similar manner to the single-word python script
 """
+
+# List vowels
+vowels = 'aeiou'
+# Pig latin
+pig = 'ay'
+# Array to store words
+word_list = ""
+# Counter
+count = 0
+# Letter storage
+letters = []
+# Get user input and convert to lower case
+phrase = raw_input("Please enter a phrase: ").lower()
+
+
+for i in phrase:
+	if i == " ":
+		count = count + 1
+		for char in letters:
+			word_list += char
+			letters = []
+		print i,  count
+	else:
+		letters.append(i)
+		print i,  count
+print word_list
+
